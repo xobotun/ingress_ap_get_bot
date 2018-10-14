a0a186c47b5f86a66d5a38762e5e0f1b77c10b02
Paul Maminov <xobotun@inbox.ru>
2018-10-14 12:14:30 +0300

    Fixed a counting bug.

--- src/main/java/com/xobotun/ingress_ap_get_bot/ApEvent.java
+++ src/main/java/com/xobotun/ingress_ap_get_bot/ApEvent.java
@@ -17,7 +17,7 @@ public enum ApEvent {
     CREATE_LINK(313, true, "Create a link"),
     FIRST_RESONATOR_DEPLOY(500 + 125, true, "Capture gray portal"),
     CONTROL_FIELD_CREATE(1250 + 313, true, "Create control field"),   // with at least one link
-    PORTAL_CAPTURE(500 + 325 + 125 * 8, true, "Fill all nodes with resonators");
+    PORTAL_CAPTURE(500 + 250 + 125 * 8, true, "Fill all nodes with resonators");
     // Hard to use IRL, decided to exclude them.
 //    LAST_RESONATOR_DEPLOY(325 + 125);
 //    CONTROL_FIELD_DESTROY(750),
