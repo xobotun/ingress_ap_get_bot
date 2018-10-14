f9b6f0126643ec689d6ec7240403f058c5074d21
Paul Maminov <xobotun@inbox.ru>
2018-10-14 12:03:38 +0300

    Now working as intended.

--- src/main/java/com/xobotun/ingress_ap_get_bot/ApEvent.java
+++ src/main/java/com/xobotun/ingress_ap_get_bot/ApEvent.java
@@ -9,15 +9,15 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ApEvent {
-    PORTAL_RECHARGE(10, false),
-    RESONATOR_UPGRADE(65, true),
-    HACK(100, true),
-    RESONATOR_DEPLOY(125, true),
-    MOD_INSTALL(125, true),
-    CREATE_LINK(313, true),
-    FIRST_RESONATOR_DEPLOY(500 + 125, true),
-    CONTROL_FIELD_CREATE(1250 + 313, true),   // with at least one link
-    PORTAL_CAPTURE(500 + 325 + 125 * 8, true);
+    PORTAL_RECHARGE(10, false, "Recharge green portal"),
+    RESONATOR_UPGRADE(65, true, "Upgrade other's resonator"),
+    HACK(100, true, "Simply hack blue portal"),
+    RESONATOR_DEPLOY(125, true, "Deploy a single resonator"),
+    MOD_INSTALL(125, true, "Install any mod"),
+    CREATE_LINK(313, true, "Create a link"),
+    FIRST_RESONATOR_DEPLOY(500 + 125, true, "Capture gray portal"),
+    CONTROL_FIELD_CREATE(1250 + 313, true, "Create control field"),   // with at least one link
+    PORTAL_CAPTURE(500 + 325 + 125 * 8, true, "Fill all nodes with resonators");
     // Hard to use IRL, decided to exclude them.
 //    LAST_RESONATOR_DEPLOY(325 + 125);
 //    CONTROL_FIELD_DESTROY(750),
@@ -27,5 +27,12 @@ public enum ApEvent {
      * How much users AP should increase under normal circumstanses.
      */
     int increaseAmount;
+    /**
+     * Whether to decrease amount of actions of this kind to reduce dullness.
+     */
     boolean applySieve;
+    /**
+     * Action description to write in the beginning of a sentence.
+     */
+    String description;
 }
--- src/main/java/com/xobotun/ingress_ap_get_bot/ApGetBot.java
+++ src/main/java/com/xobotun/ingress_ap_get_bot/ApGetBot.java
@@ -2,12 +2,16 @@ package com.xobotun.ingress_ap_get_bot;
 
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.xobotun.ingress_ap_get_bot.calculator.Calculator;
+import com.xobotun.ingress_ap_get_bot.calculator.Results;
 import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.json.JSONObject;
 import spark.Request;
 import spark.Response;
+
+import java.util.Comparator;
 
 import static spark.Spark.*;
 
@@ -30,7 +34,7 @@ class ApGetBot {
             return "helped";
         }
         if (text.startsWith("/calc")) {
-            sendMessage(chatId, "Not Implemented");
+            sendMessage(chatId, calculateApDelta(text));
             return "calculated";
         }
 
@@ -61,6 +65,57 @@ class ApGetBot {
 
     private static String badRequest() {
         return "I don't know how to respond to your request. :(";
+    }
+
+    private static String calculateApDelta(String text) {
+        String[] tokens = text.replaceAll("\\s{2}", " ").trim().split(" ");     // never can be too much escaping
+
+        //#region validate input
+        long apCountLesser;
+        long apCountGreater;
+        try {
+            apCountLesser = Long.valueOf(tokens[1]);
+            apCountGreater = Long.valueOf(tokens[2]);
+        } catch (NumberFormatException e) {
+            return "Just for you to know, AP is represented by digits only.";
+        }
+
+        if (apCountGreater < apCountLesser) {
+            long tmp = apCountGreater;
+            apCountGreater = apCountLesser;
+            apCountLesser = tmp;
+        }
+        // #endregion
+
+        Results result = Calculator.calculate(apCountLesser, apCountGreater);
+        StringBuilder response = new StringBuilder(
+                String.format("You want to increase your AP from `%d` to `%d` by `%d`.\n\n",
+                        apCountLesser,
+                        apCountGreater,
+                        apCountGreater - apCountLesser
+                )
+        );
+
+        if (result.getApNotDistributed() > 0) {
+            int lastDigitShouldBe = (int) (apCountLesser % 10 + result.getApNotDistributed()) % 10;
+            response.append(String.format("First, consider glyphing until your AP ends with `%d` digit.\n" +
+                                          "Anyway, your next steps will be like:\n\n", lastDigitShouldBe));
+        } else {
+            response.append("You just need to\n\n");
+        }
+
+        result.getNumOfEvents().entrySet().stream()
+                .sorted(Comparator.comparingInt(entry -> entry.getKey().getIncreaseAmount()))
+                .forEach(countedApEvent -> {
+                    response.append(String.format("%s `%d` times for `%d` AP gain\n",
+                            countedApEvent.getKey().getDescription(),
+                            countedApEvent.getValue(),
+                            countedApEvent.getKey().getIncreaseAmount() * countedApEvent.getValue()));
+                });
+
+
+
+        return response.toString();
     }
 }
 
